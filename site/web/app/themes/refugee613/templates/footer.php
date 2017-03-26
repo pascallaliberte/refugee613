126e01dff54c73f39b7a5b63af13960434f2256a
Eric Tran <erictran5791@gmail.com>
2017-03-26 11:17:23 -0400

    Added Vue JS, sample Ajax call

--- site/web/app/themes/refugee613/templates/footer.php
+++ site/web/app/themes/refugee613/templates/footer.php
@@ -2,4 +2,7 @@
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.2.5/vue.min.js"></script>
+  <script type="text/javascript" src="<?php echo bloginfo('template_directory');?>/assets/scripts/r613.js"></script>
+
 </footer>
